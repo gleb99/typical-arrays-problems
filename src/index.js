7d916cefaf41e4e535f9d9506d1e8651bcaef679
Gleb <gllukutin@gmail.com>
2020-03-18 22:17:30 +0300

    add: add solving of the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
-exports.min = function min (array) {
-  return 0;
+
+exports.min = function min (arr) {
+  if(arr === undefined || arr.length < 1) return 0;
+  let min = arr.sort((a, b) => a - b);
+  
+  return min[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  if(arr === undefined || arr.length < 1) return 0;
+  let max = arr.sort((a,b) => b-a);
+  return max[0];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  if(arr === undefined || arr.length < 1) return 0;
+  let length = arr.length;
+  let summ = arr.reduce((res, item) => res + item , 0);
+  return summ/length;
 }
